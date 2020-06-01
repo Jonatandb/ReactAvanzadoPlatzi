eacf0442fcaa89002a0e4027be07c18357e30b3d
Jonatandb <jonatandb@gmail.com>
2020-06-01 01:03:36 -0300

    Instalación de React y Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Empezamos :-)");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Seguimos con el curso avanzado!</h1>,
+  document.getElementById("app")
+);
