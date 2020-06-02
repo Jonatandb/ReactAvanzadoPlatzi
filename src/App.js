c66a28aaa3ac0341f2110186e6d3b68a8c05bd1e
Jonatandb <jonatandb@gmail.com>
2020-06-01 23:53:17 -0300

    Creando nuestro primer componente Category

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Category } from "./components/Category";
+
+export default function App() {
+  return (
+    <>
+      <h1>Petgram - Jonatandb</h1>
+      <Category path="/" />
+    </>
+  );
+}
