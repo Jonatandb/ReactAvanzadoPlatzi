e724c9a0b9e73ee171a784afb52f5964504e5e19
Jonatandb <jonatandb@gmail.com>
2020-06-03 21:39:35 -0300

    Creación de Hook useCategoriesData

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,18 +2,31 @@ import React, { useState, useEffect } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
 
-const ListOfCategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setShowFixed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     window
       .fetch("https://petgram-api-jonatandb.now.sh/categories")
       .then((res) => res.json())
       .then((response) => {
+        setLoading(false);
         setCategories(response);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log("useCategoriesData() -> Error:", err);
       });
   }, []);
+
+  return { categories, loading };
+}
+
+const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false);
+  const { categories, loading } = useCategoriesData();
 
   useEffect(() => {
     const onScroll = (e) => {
@@ -27,12 +40,18 @@ const ListOfCategories = () => {
   }, [showFixed]);
 
   const renderList = (fixed) => (
-    <List className={fixed ? "fixed" : ""}>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
+    <List fixed={fixed}>
+      {loading ? (
+        <Item key="loading">
+          <Category />
         </Item>
-      ))}
+      ) : (
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      )}
     </List>
   );
 
