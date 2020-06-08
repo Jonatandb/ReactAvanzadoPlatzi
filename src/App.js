654346de03ef460214549cd823e3f1ece433c5c8
Jonatandb <jonatandb@gmail.com>
2020-06-07 21:35:27 -0300

    Agregando un NavBar a nuestra app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Logo from "./components/Logo";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 
+import { NavBar } from "./components/NavBar";
+
 export default function App() {
   return (
     <>
@@ -17,6 +19,7 @@ export default function App() {
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <NavBar />
     </>
   );
 }
