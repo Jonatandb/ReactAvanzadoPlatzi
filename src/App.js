95de7e03c1d3824e5b637586ec5f80ea7dd37ba6
Jonatandb <jonatandb@gmail.com>
2020-06-17 17:51:15 -0300

    Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
-import { Router } from "@reach/router";
+import { Router, Redirect } from "@reach/router";
 
 import Logo from "./components/Logo";
 
@@ -8,39 +8,31 @@ import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
+import { NotFound } from "./pages/NotFound";
 import { NotRegisterUser } from "./pages/NotRegisterUser";
 
 import { NavBar } from "./components/NavBar";
 
-import Context from "../Context";
+import { Context } from "../Context";
 
 export default function App() {
+  const { isAuth } = useContext(Context);
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisterUser path="/login" />}
+        {!isAuth && <Redirect from="/favs" to="/login" />}
+        {!isAuth && <Redirect from="/user" to="/login" />}
+        {isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisterUser path="/favs" />
-              <NotRegisterUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
-
       <NavBar />
     </>
   );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NotFound = () => {
+  return <h1>Esta página no existe! 😢</h1>;
+};
