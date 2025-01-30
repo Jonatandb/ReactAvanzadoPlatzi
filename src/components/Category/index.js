8476288a82d383834f780918f3a14ed959abcfe4
Jonatandb <jonatandb@gmail.com>
2025-01-30 20:32:45 -0300

    Add package.json "engines->node: 22.x" Upgrade Apollo to 2.25 Upgrade Bcrypt to 5.0 Update images URL

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, Image } from "./styles";
 
 const DEFAULT_IMAGE =
-  "https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg";
+  "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=2043&auto=format&fit=crop";
 
 const Category = ({ cover = DEFAULT_IMAGE, path = "#", emoji = "?" }) => (
   <Link to={path}>
